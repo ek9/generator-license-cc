e49587a4f0c846a314e57d5e89a66141dee70b5e
ek9 <dev@ek9.co>
2017-01-07 18:39:15 +0100

    add CC-BY-ND-4.0 LICENSE

--- test/app.js
+++ test/app.js
@@ -64,4 +64,24 @@ describe('generator-license-cc:app CC-BY-SA-4.0', function () {
   });
 });
 
+describe('generator-license-cc:app CC-BY-ND-4.0', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        name: testName,
+        work: testWork,
+        year: testYear,
+        email: testEmail,
+        website: testWebsite,
+        license: testLicenses[0],
+        output: testOutput
+      }).toPromise();
+  });
 
+  it('creates CC-BY-ND-4.0 LICENSE file with correct data', function () {
+    assert.fileContent(testOutput, testHeader);
+    assert.fileContent(testOutput, 'Creative Commons Attribution-NoDerivs 4.0 International License.');
+    assert.fileContent(testOutput, 'creativecommons.org/licenses/by-na/4.0');
+    assert.fileContent(testOutput, 'Attribution-NoDerivs 4.0 International');
+  });
+});
