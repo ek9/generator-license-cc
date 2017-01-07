352c21d18e68fdd899383f5cdd6f82ef3692173c
ek9 <dev@ek9.co>
2017-01-07 19:09:09 +0100

    add test to test app.js with various options

---
+++ test/app.js
@@ -0,0 +1,117 @@
+'use strict';
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+
+var testName = 'Bob Jr';
+var testWork = 'Test Work';
+var testYear = '2016-2017';
+var testEmail = 'bob@example.com';
+var testWebsite = 'http://www.example.com';
+var testLicenses = [
+  'CC-BY-4.0'
+];
+var testOutput = 'LICENSE';
+
+var testHeaderFull =
+  testWork + ' (c) ' + testYear + ' ' + testName + ' <' + testEmail + '> (' + testWebsite + ')';
+var testHeaderNoContacts =
+  testWork + ' (c) ' + testYear + ' ' + testName;
+var testHeaderNoContactsWebsite =
+  testWork + ' (c) ' + testYear + ' ' + testName + ' <' + testEmail + '>';
+var testHeaderNoContactsEmail =
+  testWork + ' (c) ' + testYear + ' ' + testName + ' (' + testWebsite + ')';
+
+var testHeaderNoYear =
+  testWork + ' (c)  ' + testName;
+
+describe('generator-license-cc:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        name: testName,
+        work: testWork,
+        year: testYear,
+        email: testEmail,
+        website: testWebsite,
+        license: testLicenses[0],
+        output: testOutput
+      }).toPromise();
+  });
+
+  it('creates LICENSE file with correct data', function () {
+    assert.fileContent(testOutput, testHeaderFull);
+  });
+});
+
+describe('generator-license-cc:app no contact details', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        name: testName,
+        work: testWork,
+        year: testYear,
+        license: testLicenses[0],
+        output: testOutput
+      }).toPromise();
+  });
+
+  it('creates LICENSE file with correct data', function () {
+    assert.fileContent(testOutput, testHeaderNoContacts);
+  });
+});
+
+describe('generator-license-cc:app no contact details - email', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        name: testName,
+        website: testWebsite,
+        work: testWork,
+        year: testYear,
+        license: testLicenses[0],
+        output: testOutput
+      }).toPromise();
+  });
+
+  it('creates LICENSE file with correct data', function () {
+    assert.fileContent(testOutput, testHeaderNoContactsEmail);
+  });
+});
+
+describe('generator-license-cc:app no contact details - website', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        name: testName,
+        email: testEmail,
+        work: testWork,
+        year: testYear,
+        license: testLicenses[0],
+        output: testOutput
+      }).toPromise();
+  });
+
+  it('creates LICENSE file with correct data', function () {
+    assert.fileContent(testOutput, testHeaderNoContactsWebsite);
+  });
+});
+
+
+
+describe('generator-license-cc:app no contact details + no year', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        name: testName,
+        work: testWork,
+        license: testLicenses[0],
+        output: testOutput
+      }).toPromise();
+  });
+
+  it('creates LICENSE file with correct data', function () {
+    assert.fileContent(testOutput, testHeaderNoYear);
+  });
+});
+
